fe33558e9f412e488a016a98a5d0885b279492db
Bohemius1272 <jerryknyazhevsky@gmail.com>
2025-10-23 21:12:15 -0400

    first commit

---
+++ hangman.js
@@ -0,0 +1,43 @@
+var Possible_Words = ["javascript", "glass", "amazing", "pancake", "programming", "google", "developer", "apple", "python", "computer", "vault", "wizard",
+     "galaxy", "notebook", "keyboard", "route", "monitor", "laptop", "headphones", "charger", "internet"];
+
+var word = "";
+var guesses = "";
+
+function newGame() {
+
+    var randomIndex = parseInt(Math.random() * Possible_Words.length);
+    word = Possible_Words[randomIndex];
+    guesses = "";
+    updatePage();
+}
+function guessLetter() {
+    var input = document.getElementById("guess");
+    var letter = input.value;
+    guesses += letter;
+    updatePage();
+}
+function updatePage() {
+
+    var clueString = "";
+    for (var i = 0; i < word.length; i++) {
+        var currentLetter = word.charAt(i);
+        if (guesses.indexOf(currentLetter) >= 0) {
+            clueString += currentLetter + " ";
+        }
+        else
+            clueString += " _ ";
+    }
+
+    var clue = document.getElementById("clue");
+    clue.innerHTML = clueString;
+
+    var guessArea = document.getElementById("guesses");
+    guessArea.innerHTML = "Guesses: " + guesses;
+
+    var image = document.getElementById("hangmanImage");
+    image.src = "images/hangman" + guess_count + " .gif";
+
+
+}
+
